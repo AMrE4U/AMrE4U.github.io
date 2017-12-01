bfad67c035605e8b46a157cf37c02381771c19cf
“www_root” <GS-W-KS_info@usgs.gov>
2017-12-01 13:56:42 -0600

    added obstruction and new mating functions.

--- rockets/rocket.js
+++ rockets/rocket.js
@@ -2,6 +2,7 @@ function Population() {
   this.rockets = [];
   this.popsize = 25;
   this.matingpool = [];
+  this.gen = 0;
 
   for(var i = 0; i < this.popsize; i++) {
     this.rockets[i] = new Rocket();
@@ -18,6 +19,9 @@ function Population() {
     for(var i = 0; i < this.popsize; i++) {
       this.rockets[i].fitness /= maxfit;
     }
+
+    //console.log(this.rockets);
+
     this.matingpool = [];
     for(var i = 0; i < this.popsize; i++) {
       var n = this.rockets[i].fitness * 100;
@@ -34,9 +38,11 @@ function Population() {
       var parentB = random(this.matingpool).dna;
 
       var child = parentA.crossover(parentB);
+      child.mutation();
       newRockets[i] = new Rocket(child);
     }
     this.rockets = newRockets;
+    this.gen++;
   }
 
   this.run = function() {
@@ -54,21 +60,44 @@ function DNA(genes) {
     this.genes = [];
     for (var i = 0; i < lifespan; i++) {
       this.genes[i] = p5.Vector.random2D();
-      this.genes[i].setMag(0.1);
+      this.genes[i].setMag(maxforce);
     }
   }
 
   this.crossover = function(partner) {
     var newgenes = [];
-    var mid = floor(random(this.genes.length));
+    // var mid = floor(random(this.genes.length));
+    // for (var i = 0; i < this.genes.length; i++) {
+    //   if(i > mid) {
+    //     newgenes[i] = this.genes[i];
+    //   } else {
+    //     newgenes[i] = partner.genes[i];
+    //   }
+    // }
+
+    // for (var i = 0; i < this.genes.length; i++) {
+    //   newgenes[i] = p5.Vector.add(this.genes[i], partner.genes[i]);
+    //   newgenes[i].setMag(maxforce);
+    // }
+
     for (var i = 0; i < this.genes.length; i++) {
-      if(i > mid) {
+      if (random(1) < 0.5){
         newgenes[i] = this.genes[i];
       } else {
         newgenes[i] = partner.genes[i];
       }
     }
+
     return new DNA(newgenes);
+  }
+
+  this.mutation = function() {
+    for (var i = 0; i < this.genes.length; i++) {
+      if(random(1) < .001) {
+        this.genes[i] = p5.Vector.random2D();
+        this.genes[i].setMag(maxforce);
+      }
+    }
   }
 }
 
@@ -76,6 +105,8 @@ function Rocket(dna) {
   this.pos = createVector(width/2, height);
   this.vel = createVector();
   this.acc = createVector();
+  this.complete = false;
+  this.crashed = false;
   if(dna){
     this.dna = dna;
   } else {
@@ -89,15 +120,43 @@ function Rocket(dna) {
   }
 
   this.update = function() {
+
+    var d = dist(this.pos.x, this.pos.y, target.x, target.y);
+    if(!this.complete && d < 10){
+      this.complete = true;
+      this.pos = target.copy();
+      this.fitness = count;
+    }
+
+    if (!this.crashed && this.pos.x > rx && this.pos.x < rx + rw && this.pos.y > ry && this.pos.y < ry + rh) {
+      this.crashed = true;
+      this.fitness = count;
+    }
+    if (!this.crashed && this.pos.x < 0 || this.pos.x > width || this.pos.y < 0) {// || this.pos.y > height){
+      this.crashed = true;
+      this.fitness = count;
+    }
+
     this.applyForce(this.dna.genes[count]);
-    this.vel.add(this.acc);
-    this.pos.add(this.vel);
-    this.acc.mult(0);
+    if(!this.complete && !this.crashed){
+      this.vel.add(this.acc);
+      this.pos.add(this.vel);
+      this.acc.mult(0);
+      this.vel.limit(4);
+    }
   }
 
   this.calcFitness = function() {
     var d = dist(this.pos.x, this.pos.y, target.x, target.y);
-    this.fitness = map(d, 0, width, width, 0);
+    var adjDist = map(d, 0, width + height, width + height, 0);
+
+    if(this.complete){
+      this.fitness = adjDist * (lifespan/this.fitness);
+    } else if(this.crashed) {
+      this.fitness = adjDist * (this.fitness / lifespan);
+    } else {
+      this.fitness = adjDist;
+    }
   }
 
   this.show = function() {
--- rockets/sketch.js
+++ rockets/sketch.js
@@ -1,12 +1,22 @@
-var lifespan = 200;
+var lifespan = 400;
 var count = 0;
+var maxforce = 0.2;
 var target;
+var gen;
+
+var rx = 100;
+var ry = 150;
+var rw = 200;
+var rh = 10;
 
 function setup() {
     createCanvas(400, 300);
     //rocket = new Rocket();
     population = new Population();
     target = createVector(width / 2, 50);
+
+    gen = createP();
+    gen.html(0);
 }
 
 function draw() {
@@ -17,9 +27,12 @@ function draw() {
     if (count == lifespan) {
       population.evaluate();
       population.selection();
+      gen.html(population.gen);
       //population = new Population();
       count = 0;
+      //noLoop();
     }
 
+    rect(rx, ry, rw, rh);
     ellipse(target.x, target.y, 16, 16);
 }
