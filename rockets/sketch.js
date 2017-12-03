84d4bf468e76ae55c93b6e9a38548f492297509d
RJ <rjhender@ku.edu>
2017-12-03 15:41:38 -0600

    Changed rockets to full screen.

--- rockets/sketch.js
+++ rockets/sketch.js
@@ -4,19 +4,22 @@ var maxforce = 0.2;
 var target;
 var gen;
 
-var rx = 100;
-var ry = 150;
-var rw = 200;
+var rx, ry, rw;
 var rh = 10;
 
 function setup() {
-    createCanvas(400, 300);
+    //createCanvas(400, 300);
+    createCanvas(windowWidth, windowHeight);
     //rocket = new Rocket();
     population = new Population();
     target = createVector(width / 2, 50);
+    
+    rw = floor(width / 3);
+    rx = (width / 2) - (rw / 2);
+    ry = floor(height / 3);
 
-    gen = createP();
-    gen.html(0);
+    //gen = createP();
+    //gen.html(0);
 }
 
 function draw() {
@@ -27,7 +30,7 @@ function draw() {
     if (count == lifespan) {
       population.evaluate();
       population.selection();
-      gen.html(population.gen);
+      //gen.html(population.gen);
       //population = new Population();
       count = 0;
       //noLoop();
@@ -35,4 +38,6 @@ function draw() {
 
     rect(rx, ry, rw, rh);
     ellipse(target.x, target.y, 16, 16);
+    fill(255);
+    text("Generation: " + population.gen, 10, 20);
 }
